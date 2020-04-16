ce6e3719157310ef4511ca6278d082aa81291226
Haylie Goh <h.goh@deakin.edu.au>
2020-04-16 20:56:53 +1000

    link to html completed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 // ===========================================================
 var express = require("express");
+var path = require("path");
 
 var app = express();
 var PORT = 3000;
@@ -8,29 +9,27 @@ var PORT = 3000;
 // Data
 // ===========================================================
 
-
 // Routes
 // ===========================================================
 
-app.get("/", function(req, res) {
-  res.send("Welcome to Hot Resturant!");
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "frontend/home.html"));
 });
 
 // What does this route do?
-app.get("/api/make", function(req, res) {
+app.get("/reservation", function (req, res) {
   //return res.json(characters);
-  res.send("Make a reservation");
+  res.sendFile(path.join(__dirname, "./frontend/reservation.html"));
 });
 
 // What does this route do?
-app.get("/api/table", function(req, res) {
-
+app.get("/table", function (req, res) {
+  res.sendFile(path.join(__dirname, "frontend/table.html"));
   // What does this code do?
-  return res.send("View reservation");
 });
 
 // Listener
 // ===========================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
